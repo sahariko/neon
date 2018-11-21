6fe8b61dd8cd91c439b320fb89b53b2c0650efc3
Sahar Brodbeker <saharonb@gmail.com>
2018-11-21 23:44:53 +0200

    Update README with screenshot

--- installation.sh
+++ installation.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 mkdir -p ~/.oh-my-zsh/custom/themes/
-wget -xqO ~/.oh-my-zsh/custom/themes/neon.zsh-theme https://raw.githubusercontent.com/sahariko/neon/master/neon.zsh-theme
+wget --no-check-certificate -P ~/.oh-my-zsh/custom/themes/ https://raw.githubusercontent.com/sahariko/neon/master/neon.zsh-theme
 sed -i.bak 's/^[[:space:]]*ZSH_THEME=.*/ZSH_THEME="neon"/' ~/.zshrc
